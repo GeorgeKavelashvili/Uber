adebd0e66fa57f934766415ef015b79529e06bde
GeoulMan <gkavelashvili2@gmail.com>
2023-04-25 17:20:46 +0400

    fixed bug

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Router from './src/navigation/root';
-import 'react-native-gesture-handler';
+
 export default class App extends React.Component {
   render() {
-    <Router />;
+    return <Router />;
   }
 }
